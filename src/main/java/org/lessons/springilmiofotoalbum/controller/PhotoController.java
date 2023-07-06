6854f4fcd00e5e2aa47988bcba0478be3eb781b6
marcantonio99 <marcantonio8799@gmail.com>
2023-07-06 15:50:13 +0200

    detail and fix homepage

--- src/main/java/org/lessons/springilmiofotoalbum/controller/PhotoController.java
+++ src/main/java/org/lessons/springilmiofotoalbum/controller/PhotoController.java
@@ -3,12 +3,16 @@ package org.lessons.springilmiofotoalbum.controller;
 import org.lessons.springilmiofotoalbum.model.Photo;
 import org.lessons.springilmiofotoalbum.repository.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/photos")
@@ -24,5 +28,18 @@ public class PhotoController {
         model.addAttribute("photoList", photos);
 
         return "/photos/list";
+    }
+
+    @GetMapping("/{id}")
+    public String detail(@PathVariable("id") Integer photoId, Model model){
+
+        Optional<Photo> result = photoRepository.findById(photoId);
+        if (result.isPresent()){
+            model.addAttribute("photo", result.get());
+
+            return "/photos/detail";
+        }else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
